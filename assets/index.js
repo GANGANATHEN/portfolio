79ab4d74b1c835a49966d5d390c3df47d4573780
Ganganathen <ganganathan.m@outlook.com>
2026-06-12 15:35:14 +0530

    Fix: Pushing all files to repository

---
+++ assets/index.js
@@ -0,0 +1,76 @@
+import logo from "./logo.svg";
+import pydev from "./backend.png";
+import jadev from "./creator.png";
+import rjsdev from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import python from "./tech/python.png";
+import java from "./tech/java.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import redhat from "./company/redhat.png";
+import ibm from "./company/ibm.png";
+import aws from "./company/aws.png";
+import cisco from "./company/CISCO.png";
+import uomcodl from "./company/UOMcodl.png";
+import devtown from "./company/devtown.png";
+import eminent from "./company/eminent.jpeg";
+
+import appleimg from "./prjectimg/Appleweb.png";
+import calci from "./prjectimg/calci.png";
+import ganganathan from "./prjectimg/ganganathan.png";
+import sociallink from "./prjectimg/sociallink.png";
+import qrcode from "./prjectimg/qrcode.png";
+import scoreboard from "./prjectimg/cricket.png";
+import grade from "./prjectimg/grademark.png"
+
+export {
+  logo,
+  rjsdev,
+  jadev,
+  pydev,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  python,
+  java,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  redhat,
+  ibm,
+  aws,
+  cisco,
+  devtown,
+  eminent,
+  uomcodl,
+  calci,
+  ganganathan,
+  appleimg,
+  sociallink,
+  qrcode,
+  scoreboard,
+  grade,
+};
